7a510f5a6732b4c6c01a82d1df54c2390a04e8de
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-08-31 18:36:07 +0300

    ajax a

--- components/List.js
+++ components/List.js
@@ -1,45 +1,44 @@
-import {FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList, View, Text, Image} from 'react-native';
 
-import ListItem from './ListItem';
-
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
+const url =
+  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
 
 const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (error) {
+      console.error('Error loading media:', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  const keyExtractor = (_, index) => index.toString();
+
+  const renderItem = ({item}) => (
+    <View style={{padding: 16}}>
+      <Image
+        source={{uri: item.thumbnails.w160}}
+        style={{width: 160, height: 160}}
+      />
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  );
+
   return (
     <FlatList
       data={mediaArray}
-      renderItem={({item}) => <ListItem singleMedia={item} />}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
     />
   );
 };
