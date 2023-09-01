e6218e5750adb5f95acc207ed8cad825debe35f4
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-09-01 09:42:04 +0300

    ajax-b and custom hooks

--- components/List.js
+++ components/List.js
@@ -1,44 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {FlatList, View, Text, Image} from 'react-native';
-
-const url =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+import {FlatList} from 'react-native';
+import ListItem from './ListItem';
+import {useMedia} from '../hooks/ApiHooks';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(url);
-      const json = await response.json();
-      setMediaArray(json);
-    } catch (error) {
-      console.error('Error loading media:', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
-  const keyExtractor = (_, index) => index.toString();
-
-  const renderItem = ({item}) => (
-    <View style={{padding: 16}}>
-      <Image
-        source={{uri: item.thumbnails.w160}}
-        style={{width: 160, height: 160}}
-      />
-      <Text>{item.title}</Text>
-      <Text>{item.description}</Text>
-    </View>
-  );
+  const {mediaArray} = useMedia();
 
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
 };
---
+++ hooks/ApiHooks.js
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react';
+import {apiUrl} from '../utils/app-config';
+import {doFetch} from '../utils/functions';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const json = await doFetch(apiUrl + 'media');
+      // console.log(json);
+      const mediaFiles = await Promise.all(
+        json.map(async (item) => {
+          const fileData = await doFetch(apiUrl + 'media/' + item.file_id);
+          // console.log('fileData', fileData);
+          return fileData;
+        }),
+      );
+      // console.log(data);
+      setMediaArray(mediaFiles);
+    } catch (error) {
+      console.error('loadMedia failed', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  return {mediaArray};
+};
+
+export {useMedia};
