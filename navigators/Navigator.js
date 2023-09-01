18e4790b1ae8d7ce411a365f2e8437c6bfe24306
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-09-01 11:08:23 +0300

    dummy-login

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Single from '../views/Single';
+import Login from '../views/Login';
+import {MainContext} from '../contexts/MainContext';
 
 const Tab = createBottomTabNavigator();
+
 const Stack = createNativeStackNavigator();
+
+const Stackscreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
+  return (
+    <Stack.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen name="Tabs" component={Tabscreen} />
+          <Stack.Screen name="Single" component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
+    </Stack.Navigator>
+  );
+};
 
 const Tabscreen = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home}></Tab.Screen>
-      <Tab.Screen name="Profile" component={Profile}></Tab.Screen>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
-  );
-};
-
-const Stackscreen = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Tabs"
-        component={Tabscreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen name="Single" component={Single} />
-    </Stack.Navigator>
   );
 };
 
