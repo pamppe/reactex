f915b7bbdd8333321510df3011ffd53a4bcfe44a
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-09-03 15:22:55 +0300

    tuunattu

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,4 +1,5 @@
-import {Image, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
 const ListItem = ({singleMedia}) => {
@@ -7,13 +8,13 @@ const ListItem = ({singleMedia}) => {
       onPress={() => {
         console.log('touched!', singleMedia.title);
       }}
+      style={styles.container}
     >
-      <Image
-        style={{width: 100, height: 100}}
-        source={{uri: singleMedia.thumbnails.w160}}
-      />
-      <Text>{singleMedia.title}</Text>
-      <Text>{singleMedia.description}</Text>
+      <Image style={styles.image} source={{uri: singleMedia.thumbnails.w160}} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{singleMedia.title}</Text>
+        <Text style={styles.description}>{singleMedia.description}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -21,5 +22,34 @@ const ListItem = ({singleMedia}) => {
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#668', // Background color
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  image: {
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10, //
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  description: {
+    fontSize: 14,
+    color: 'black',
+  },
+});
 
 export default ListItem;
