90580cbe0d3b577699048aacd055b51532dff542
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-09-04 14:06:05 +0300

    style

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
-import {Platform} from 'react-native';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import List from '../wpms-examples232/components/List';
+import List from './components/List';
+
+import {Settings} from 'react-native-feather';
 
 const App = () => {
+  const image = {uri: 'http://placekitten.com/2039/1920'};
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <ImageBackground style={styles.header} source={image}>
+          <Text style={styles.headerTitle}>Kassipojad</Text>
+          <Settings
+            stroke="white"
+            fill="none"
+            width={32}
+            height={32}
+            style={styles.icon}
+          />
+        </ImageBackground>
+      </View>
+
       <List />
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -14,11 +36,39 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'pink',
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    elevation: 10,
+  },
+
+  header: {
+    height: 250,
+  },
+
+  headerTitle: {
+    position: 'absolute',
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    bottom: 10,
+    backgroundColor: 'pink',
+  },
+
+  TouchableOpacity: {
+    marginVertical: 2,
+    padding: 5,
+  },
+  icon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
   },
 });
 
--- components/ListItem.js
+++ components/ListItem.js
@@ -2,20 +2,24 @@ import React from 'react';
 import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
-const ListItem = ({singleMedia}) => {
+const ListItem = (props) => {
   return (
-    <TouchableOpacity
-      onPress={() => {
-        console.log('touched!', singleMedia.title);
-      }}
-      style={styles.container}
-    >
-      <Image style={styles.image} source={{uri: singleMedia.thumbnails.w160}} />
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>{singleMedia.title}</Text>
-        <Text style={styles.description}>{singleMedia.description}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.TouchableOpacity}>
+        <View style={styles.box}>
+          <Image
+            style={styles.Image}
+            source={{uri: props.singleMedia.thumbnails.w160}}
+          />
+          <View style={styles.View}>
+            <Text style={styles.Title}>{props.singleMedia.title}</Text>
+            <Text style={styles.Description}>
+              {props.singleMedia.description}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -27,27 +31,38 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#668', // Background color
+    backgroundColor: 'pink', // Background color
     padding: 10,
     borderRadius: 10,
     marginBottom: 10,
   },
-  image: {
-    width: 120,
-    height: 100,
-    borderRadius: 10,
-    marginRight: 10, //
+  box: {
+    flexDirection: 'row',
+    borderRadius: 15,
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
   },
-  textContainer: {
-    flex: 1,
+  Image: {
+    width: 150,
+    height: 250,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  View: {
+    width: '60%',
+    marginLeft: 20,
   },
   title: {
     fontSize: 16,
     fontWeight: 'bold',
     color: '#333',
   },
-  description: {
+  Description: {
+    marginTop: 5,
+    marginBottom: 5,
     fontSize: 14,
+    margin: 5,
     color: 'black',
   },
 });
